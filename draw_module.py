891ea77459e720f4706082cf7cf1dd3a5c70c9f6
TA <appulegugure@gmail.com>
2022-02-11 14:14:42 +0900

    Add:draw module Add:main initial

---
+++ draw_module.py
@@ -0,0 +1,73 @@
+"""import"""
+"""
+import asyncio
+import io
+import glob
+import os
+import sys
+import time
+import uuid
+"""
+import requests
+
+"""
+from dotenv import load_dotenv
+import json
+from urllib.parse import urlparse
+"""
+from io import BytesIO
+# To install this module, run:
+# python -m pip install Pillow
+from PIL import Image, ImageDraw
+
+"""
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person
+"""
+
+
+def getRectangle(faceDictionary):
+    rect = faceDictionary.face_rectangle
+    left = rect.left
+    top = rect.top
+    right = left + rect.width
+    bottom = top + rect.height
+
+    return ((left, top), (right, bottom))
+
+
+def getRectangle_ver2(faceDictionary):
+    rect = faceDictionary
+    left = rect.left
+    top = rect.top
+    right = left + rect.width
+    bottom = top + rect.height
+
+    return ((left, top), (right, bottom))
+
+
+def drawFaceRectangles():
+    # Download the image from the url
+    response = requests.get(single_face_image_url)
+    img = Image.open(BytesIO(response.content))
+
+    # For each face returned use the face rectangle and draw a red box.
+    print('Drawing rectangle around face... see popup for results.')
+    draw = ImageDraw.Draw(img)
+    for face in detected_faces:
+        draw.rectangle(getRectangle(face), outline='red')
+
+    # Display the image in the default image browser.
+    img.show()
+
+
+def drawFaceRectangles_ver2(image_11, array_11):
+    img = Image.open(image_11)
+    # For each face returned use the face rectangle and draw a red box.
+    print('Drawing rectangle around face... see popup for results.')
+    draw = ImageDraw.Draw(img)
+    draw.rectangle(getRectangle_ver2(array_11), outline='red')
+
+    # Display the image in the default image browser.
+    img.show()
--- main.py
+++ main.py
@@ -1,3 +1,32 @@
+"""import"""
+import os
+
+# To install this module, run:
+# python -m pip install Pillow
+from azure.cognitiveservices.vision.face import FaceClient
+from dotenv import load_dotenv
+from msrest.authentication import CognitiveServicesCredentials
+
+"""
+initial section
+
+・load env
+・define Key and Endpoint 
+・Create Face Client instance　-> face_client
+
+"""
+
+"""load env"""
+load_dotenv()
+
+"""define KEY and ENDPOINT"""
+KEY = os.environ.get("KEY")
+ENDPOINT = os.environ.get("ENDPOINT")
+
+"""Create Face Client instance"""
+face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+
+
 def main():
     pass
 
